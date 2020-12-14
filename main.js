daf3e727f63485791f362ee74bffe2d6417b9978
Mabel <65023834+Meepje@users.noreply.github.com>
2020-12-14 16:07:07 +0100

    Changed data
    Changed data, so when id is filled in, it gives back the corresponding video.
    
    When putting in id that doesn't exists (for exmaple 17), you get back 'undefined'.

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ function getPost() {
         con=numberEntered;
 
             div.innerHTML = `
-                <p class="card-video">${data[con].videoId}</p>
+                <p class="card-video">${data[0][numberEntered]}</p>
             `        
 
-            var myPlayer = new bluebillywig.Player( "http://demo.bbvms.com/p/default/c/" + data[con].videoId + ".json", {
+            var myPlayer = new bluebillywig.Player( "http://demo.bbvms.com/p/default/c/" + data[0][numberEntered] + ".json", {
                     target : document.getElementById("myPlayerDiv"),
                     autoPlay : "false"
             });
@@ -26,8 +26,6 @@ function getPost() {
         console.log(error);
     })
 }
-
-// Embed the player in the element "myPlayerDiv". Other playout settings (like width, height, autoPlay) can also be overruled by including them in the object passed as a second parameter:
 
 
 
