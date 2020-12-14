f8d6b1fe22d03e98d1adb99057d29e56294586f1
Mabel <65023834+Meepje@users.noreply.github.com>
2020-12-14 15:14:02 +0100

    input box + test video link
    Created an input box. By input of number in box, read this input, then show correlating videoId.
    Test if player link would work this way.
    
    Note: doesn't get the id number, but the array number.
    
    When clicked in 'get post' again, previous video doesn't disappear. Next video is put below previous one.

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ var con=0;
 var div= document.getElementById("cardDiv");
 
 function getPost() {
+    var numberEntered = document.getElementById("id").value; //Read input
+
     fetch ('data.json')
     .then((res)=>{
         return res.json();
     })
     .then((data)=>{
-        for (let index = 0; index < 1; index++) {
+        con=numberEntered;
+
             div.innerHTML = `
             <div class="card col-3 m-1 mx-auto">
                 <div class="card-body">
@@ -18,7 +21,11 @@ function getPost() {
             </div>
             `        
             con=con+1;
-        }
+
+            var myPlayer = new bluebillywig.Player( "http://demo.bbvms.com/p/default/c/" + numberEntered + ".json", {
+                    target : document.getElementById("myPlayerDiv"),
+                    autoPlay : "false"
+            });
     })
 
     .catch((error)=>{
@@ -27,9 +34,6 @@ function getPost() {
 }
 
 // Embed the player in the element "myPlayerDiv". Other playout settings (like width, height, autoPlay) can also be overruled by including them in the object passed as a second parameter:
-var myPlayer = new bluebillywig.Player( "http://demo.bbvms.com/p/default/c/3836075.json", {
-        target : document.getElementById("myPlayerDiv"),
-        autoPlay : "false"
-});
+
 
 
